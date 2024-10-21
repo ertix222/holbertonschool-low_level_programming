bb724305da0f966cfaff0203de96f865da09a835
ertix222 <ertix222@gmail.com>
2024-10-21 13:23:26 +0000

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always Return 0 (success)
+ */
+
 int main(void) {
   printf("with proper grammar, but the outcome is a piece of art,"
   return 0;
