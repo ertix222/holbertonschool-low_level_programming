926189fb303f96921b0656f99278288f0257ff7c
ertix222 <ertix222@gmail.com>
2024-10-21 14:58:36 +0000

    size bits

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,14 +3,15 @@
 /*
  * main - Entry main of the program
  *
- * return: Always return 0 (success) 
+ * return: Always return 0 (success)
  */
 
 int main(void)
-{ 
-printf("Size of a char: 1 byte(s)\n");    
+{
+printf("Size of a char: 1 byte(s)\n");   
 printf("Size of an int: 4 byte(s)\n");
-    if (sizeof(void*) == 4) {
+if (sizeof(void*) == 4)
+{
 printf("Size of a long int: 4 byte(s)\n");
     } else if (sizeof(void*) == 8) {       
 printf("Size of a long int: 8 byte(s)\n");
