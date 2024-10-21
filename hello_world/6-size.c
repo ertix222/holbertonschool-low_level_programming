6cb577dce449f147fcaa403cac30711df6737521
ertix222 <ertix222@gmail.com>
2024-10-21 14:41:39 +0000

    size bits

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,15 +8,15 @@
 
 int main(void)
 { 
-  printf(Size of a char: 1 byte(s));
+  printf(Size of a char: 1 byte(s);
     
-  printf(Size of an int: 4 byte(s));
+  printf(Size of an int: 4 byte(s);
     if (sizeof(void*) == 4) {
-  printf(Size of a long int: 4 byte(s));
+  printf(Size of a long int: 4 byte(s);
 	 else(      
-  printf(Size of a long int: 8 byte(s));
+  printf(Size of a long int: 8 byte(s);
 				    
-  printf(Size of a long long int: 8 byte(s));
+  printf(Size of a long long int: 8 byte(s);
   printf(Size of a float: 4 byte(s)
 	 return (0);
 }
