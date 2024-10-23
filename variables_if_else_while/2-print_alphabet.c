76b99ac23341698fb3492304d3d9597befa84509
ertix222 <ertix222@gmail.com>
2024-10-23 07:16:49 +0000

    a to z

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main- alphabet in lower
+ *
+ *Return always 0 (success)
+ */
+
+int main(void)
+{
+for (char c = 'a' ; c <= 'z'; c++){
+putchar(c);
+}
+putchar ('\n');
+return 0;
+}
