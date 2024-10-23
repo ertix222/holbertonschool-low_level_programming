8a05ae2b908fe9c7d5d3bf84e5f979ac2c076627
ertix222 <ertix222@gmail.com>
2024-10-23 09:11:39 +0000

    0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main- 0 to 9
+ *Return: always (success)
+ */
+
+int main(void)
+{
+char c;
+for (c = '0' ; c <= '9'; c++)
+{
+putchar(c);
+
+}
+putchar('\n');
+return (0);
+}
