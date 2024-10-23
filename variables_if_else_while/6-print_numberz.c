3d12a5ab320d8a42ee95a30827cd725cc56ff220
ertix222 <ertix222@gmail.com>
2024-10-23 11:31:40 +0000

    without char this time

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main- double alphabet
+ *Return: always (success)
+ */
+
+int main(void)
+{
+int = c;
+for (c = '0' ; c <= '9'; c++)
+{
+putchar(c + '0');
+}
+ putchar(+0)
+putchar('\n');
+return (0);
+}
