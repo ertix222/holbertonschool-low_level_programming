c4aa15fe20d62895136ce705a364ca183a0e62b0
ertix222 <ertix222@gmail.com>
2024-10-25 08:27:09 +0000

    prt alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet
+ *
+ *Return: always 0
+ */
+
+int print_alphabet(void)
+
+
+{
+int i;
+
+for(i = 'a' ; i <= 'z'; i++);
+{
+_putchar(i);
+}
+_putchar('\n');
+}
