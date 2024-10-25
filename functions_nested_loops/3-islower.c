f8c0695602d5b7aef2bf741289192936e9f1af24
ertix222 <ertix222@gmail.com>
2024-10-25 11:31:34 +0000

    lower case

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *islower- check if carac is lower or not
+ *
+ *Return: always
+ */
+
+/**  if lower case return 1 else return 0
+ */
+
+int _islower(int c)
+
+{
+
+  if (c > 96 && c < 123)
+{
+  return (1);
+    }
+  else
+{
+  return (0);
+    }
+}
