90eefe72fa67ea2dbdc24e8f78dad9b4c830e86a
ertix222 <ertix222@gmail.com>
2024-10-25 13:52:43 +0000

    more 0 or less

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,28 +9,22 @@
 int print_sign(int n)
 
 {
-  if (n > 0)
-    {
-  _putchar(',');
-  _putchar(' ');
-  _putchar(n + '0');
-  _putchar('\n');
-  return (1);
-    }
-  else if (n == 0)
-    {
-  _putchar(',');
-  _putchar(' ');
-  _putchar(n + '0');
-  _putchar('\n');
-  return (0);
-    }
-  else
-    {
-  _putchar(',');
-  _putchar(' ');
-  _putchar(n + '0');
-  _putchar('\n');
-	 }
-return (-1);
+if (n > 0)
+{
+_putchar('-');
+return (1);
+_putchar('\n')
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+_putchar('\n');
+}
+else
+{
+_putchar('-');
+return (-1)
+_putchar('\n');
+}
 }
