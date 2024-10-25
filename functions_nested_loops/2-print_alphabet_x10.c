9a04f31ab00d62aa96c2db6d9d08e673630c7912
ertix222 <ertix222@gmail.com>
2024-10-25 09:26:23 +0000

    again eand again alphabets

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 {
 int i;
 int a;
- for(a = 1 ; a <=  10; a++)
+for(a = 1 ; a <=  10; a++)
 {
 for (i = 'a' ; i <= 'z'; i++)
 {
 _putchar(i);
 }
-_putchar('\n'); 
+_putchar('\n');
 }
 }
