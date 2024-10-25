bb3dacc5b8afcc62b3ff57c15539b59984233c8e
ertix222 <ertix222@gmail.com>
2024-10-25 09:20:47 +0000

    again eand again alphabets

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10- print a to z 10 times
+ *
+ *Return: always 0
+ */
+
+void print_alphabet_x10(void);
+
+{
+int i;
+int a;
+ 
+for(a = 1 ; a <=  10; a++)
+{
+for (i = 'a' ; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n'); 
+}
+}
