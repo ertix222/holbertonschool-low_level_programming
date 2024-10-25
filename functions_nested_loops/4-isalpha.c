1db38cfc47761a73db72bda607816bb4e61ed8d1
ertix222 <ertix222@gmail.com>
2024-10-25 12:10:49 +0000

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,15 +2,14 @@
 
 /**
  *_islower- check if carac is lower or not
- *@c: 
+ *@c: check if carac is a letter or other thing
  *Return: always
  */
-
 
 int _islower(int c)
 
 {
-if (c > 96 && c < 123 || c > 64 && c < 91)
+if ((c > 96 && c < 123) || (c > 64 && c < 91))
 {
 return (1);
 }
