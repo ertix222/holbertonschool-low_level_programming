dd860055769aae5a9ab66ac9527a51801651fedf
ertix222 <ertix222@gmail.com>
2024-10-27 12:53:55 +0000

    all times tables..

---
+++ functions_nested_loops/9-times_tbles.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - start at 0 prints the 9 times table
+ */
+
+void times_tables(void)
+
+{
+  int a, m, s;
+
+  for(a = 0; a < 10; a++)
+    {
+      for (m = 0; m < 10; m++)
+	{
+	  s = a * m;
+	  if (m == 0)
+	    {
+	      _putchar(s + '0');
+	    }
+	  if (s < 10 && m != 0)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar(' ');
+	      _putchar(s + '0');
+	    }else if (s >= 10)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar(s / 10 + '0');
+	      _putchar(s % 10 + '0');
+	    }
+	}
+    }
+
+}
