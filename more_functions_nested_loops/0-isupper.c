03fb5aeefb557c6ae3df8a1031ee84050e22ab0e
ertix222 <ertix222@gmail.com>
2024-10-28 08:30:35 +0000

    upper or lower

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper- if the charac is a uppercase 1 else is 0
+ *@c: euh yes
+ *description: oui
+ *Returns: 1 pour tout
+ */
+
+int _isupper(int c)
+{
+  if (c >= 'A' && c <= 'Z')
+    returns (1);
+  else
+    returns (0);
+}
