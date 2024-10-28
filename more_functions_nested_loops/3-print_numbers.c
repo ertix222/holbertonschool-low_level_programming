62ef88dc5af2d867af1551d7460ba66b411b9e61
ertix222 <ertix222@gmail.com>
2024-10-28 09:07:05 +0000

    0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers- write numbers 0 to 9
+ *description: oui
+ *Return: num 0 to 9
+ */
+
+void print_numbers(void)
+
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('_n');
+}
