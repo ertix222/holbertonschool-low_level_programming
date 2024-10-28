37e532645873cb834af005e6b01891496f2c727a
ertix222 <ertix222@gmail.com>
2024-10-28 08:39:52 +0000

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_isdigit- it's a digit so type 1 else type 0
+ *@c: euh yes
+ *description: oui
+ *return: need digit
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
