71cad8e52499663c326197c8d2c36b65d99eeaa1
ertix222 <ertix222@gmail.com>
2024-10-28 09:46:53 +0000

    0 to 14 x10

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *more_numbers- print 0 to 14 more time
+ *description: oui
+ */
+
+void more_numbers(void)
+
+{
+int i;
+int a;
+for (a = 1 ; a <= 10; a++)
+{
+for (i = "0" ; i <= "14"; i++)
+{
+_putchar(i);
+}
+_putchar('\n').
+}
+}
