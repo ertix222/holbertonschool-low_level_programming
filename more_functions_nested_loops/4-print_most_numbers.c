96165f985a6ffcb8214082cd02841b48c3ab01b8
ertix222 <ertix222@gmail.com>
2024-10-28 09:35:49 +0000

    bye 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,10 @@ void print_most_numbers(void)
 int i;
 for (i = '0'; i <= '9'; i++)
 {
-if (c != '2' && i != '4')
+if (i != '2' && i != '4')
 {
 _putchar(i);
 }
 }
 _putchar('\n');
-return (0);
 }
