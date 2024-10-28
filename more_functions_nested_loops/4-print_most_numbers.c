f1052909a86b7b71f4f46d274b340924c1b0d5ee
ertix222 <ertix222@gmail.com>
2024-10-28 09:33:46 +0000

    bye 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_most_numbers- write 0 to 9 
+ *description: avoid 2 and 4
+ *Return: 0 to 9 
+ */
+
+void print_most_numbers(void)
+
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+if (c != '2' && i != '4')
+{
+_putchar(i);
+}
+}
+_putchar('\n');
+return (0);
+}
