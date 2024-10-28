7caa91f988315511b72cf7c4cc0cc8b615fd3062
ertix222 <ertix222@gmail.com>
2024-10-28 15:14:05 +0000

    #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *print_square- print '#' to form a square 
+ *description: look up
+ */
+
+void print_square(int size);
+
+{
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++);
+{
+for (j = 0; j < i; j++);
+{
+_putchar('#');
+}
+{
+_putchar('\n');
+}
+}
+}
+}
