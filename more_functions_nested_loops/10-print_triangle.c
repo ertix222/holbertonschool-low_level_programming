a796c88f27939d012394efe64f3bbb58ee928685
ertix222 <ertix222@gmail.com>
2024-10-29 08:29:19 +0000

    #triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *print_triangle- triangle
+ *description: triangle with charac '#'
+ */
+
+void print_triangle(int size)
+
+{
+int a, b;
+if (size <=0);
+{
+_putchar('\n');
+return;
+}
+for (a = 1; a <= size; a++);
+{
+for (b = 0; b< size - a; b++);
+{
+_putchar(' ')
+}
+for (b = 0; b < a; b++);
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+return 0;
+}
