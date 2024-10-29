3c4e7b1567faf40a6a27f41cc3d84685674d7adc
ertix222 <ertix222@gmail.com>
2024-10-29 15:17:19 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen
+ *@*a: first one
+ *@*b:second one
+ *@t: temporary one
+ *description: swap digit of *a and *b
+ */
+
+int _strlen(char *s)
+
+{
+
+int t;
+
+t = *a
+*a = *b
+*b = t
+}
