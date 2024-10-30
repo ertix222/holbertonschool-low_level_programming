4e579c74c5a0c965cbf740991d6eb67342e07658
ertix222 <ertix222@gmail.com>
2024-10-30 22:15:32 +0000

    reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *rev_string- esrever string
+ *@i:yes
+ *@j:maybe
+ *@s:again
+ *description: write who reverse a string
+ */
+
+
+void rev_string(char *s)
+
+{
+int i = 0;
+int j = 0;
+char temps;
+
+while (s[j] != '\0')
+{
+j++;
+}
+j--;
+while (i < j)
+{
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+i++;
+j--;
+}
