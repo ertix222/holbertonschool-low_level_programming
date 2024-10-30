c18f7bb6211e268d548c23eb19f933be259eb379
ertix222 <ertix222@gmail.com>
2024-10-30 11:14:42 +0000

    fear of computers

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts- write like put
+ *@str:yes
+ *description: write what poitn *str
+ */
+
+
+void _puts(char *str)
+
+{
+int i = 0;
+while (str[i] != '\0')
+_putchar(str[i])
+i++;
+}
