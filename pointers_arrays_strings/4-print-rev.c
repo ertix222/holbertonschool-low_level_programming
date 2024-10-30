815dc0c8165038285125f40357ea23d3cf7d8932
ertix222 <ertix222@gmail.com>
2024-10-30 13:37:27 +0000

    fear of computers

---
+++ pointers_arrays_strings/4-print-rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_rev- taht ekil etirw
+ *@s:yes
+ *description: write in esrever
+ */
+
+
+void print_rev(char *s)
+
+{
+int i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+i--;
+while (str[i] != str)
+{
+_putchar(str[i]);
+i--
+}
+_putchar(str[i]);
+{
+_putchar('\n');
+}
