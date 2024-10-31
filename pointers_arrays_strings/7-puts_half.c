0d8644ffff40bb47a7bd74cb167e87f2042410bd
ertix222 <ertix222@gmail.com3>
2024-10-31 13:34:07 +0000

    half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,24 +9,20 @@
 void puts_half(char *str)
 
 {
-  int i = 0;
-  
-while (str[i] != '\0')
-  {
-    i++
-  }
- if (i % 2 == 0)
-     {
-       i = i /2;
-       _putchar(str[i]);
-     }
- else
-   {
-     i = i-1 /2;
-     _putchar(str[i]);
-   }
-}
+int i = 0;
 
- 
-//faire une integration qui calcule la length du string//
-//a la fin diviser le resultat de la length et si impair utiliser > n = (length_of_the_string - 1) / 2 //
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+i = i / 2;
+_putchar(str[i]);
+}
+else
+{
+i = i - 1 / 2;
+_putchar(str[i]);
+}
+}
