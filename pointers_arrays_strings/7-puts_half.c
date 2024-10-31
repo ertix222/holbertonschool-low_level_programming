ffc793be38547639d06796818ec24c3b0aaec21e
ertix222 <ertix222@gmail.com3>
2024-10-31 13:57:41 +0000

    half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -19,12 +19,18 @@ if (i % 2 == 0)
 {
 i = i / 2;
 while (str[i] != '\0')
-_putchar(str[i]);
+{
+_putchar(str[i])
 i++;
+}
 }
 else
 {
 i = i - 1 / 2;
+while (str[i] != '\0')
+{
 _putchar(str[i]);
+i++;
+}
 }
 }
