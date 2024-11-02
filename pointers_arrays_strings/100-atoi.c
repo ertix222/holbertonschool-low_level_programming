0a50035cb28a4b1356ba22d9037dc5d0e2b6c722
ertix222 <ertix222@gmail.com>
2024-11-02 14:37:07 +0000

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ *_atoi- convert string to integer
+ *@s: string to be convert
+ *
+ */
+
+int _atoi(char *s)
+
+// etape 1 : Ignorer les caracteres non numeriques //
+// etape 2 : Gerer les signes + et - //
+// etape 3 : Convertir les chiffres en entier //
+// etape 4 : Gerer les debordements //
+// etape 5 : Retourner le resultat //
+  
+{
+  int a, b, c, d, e, digit;
+
+  a = 0;
+  b = 0;
+  c = 0;
+  d = 0;
+  e = 0;
+  digit = 0;
+
+  while (s[d] != '\0')
+    d ++;
+  while(a < d && e == 0)
+    {
+      if (s[a] == '-')
+	++b;
+      if (s[a] >= '0' && s[a] <= '9')
+	{
+	  digit = s[a] - '0';
+	  if (b % 2)
+	    digit = -digit;
+	  c = c * 10 + digit;
+	  e = 1:
+	    if (s[a + 1] < '0' || s[a + 1] > '9')
+	      break;
+	    e = 0:
+		}
+      a++;
+	}
+  if (e == 0)
+    return(0);
+
+      return(c);
+}
