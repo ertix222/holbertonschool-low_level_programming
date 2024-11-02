fe996dbb498320d744b815bec9ac4d28e6ef4f36
ertix222 <ertix222@gmail.com>
2024-11-02 14:40:13 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,36 +9,36 @@
 int _atoi(char *s)
   
 {
-  int a, b, c, d, e, digit;
+int a, b, c, d, e, digit;
 
-  a = 0;
-  b = 0;
-  c = 0;
-  d = 0;
-  e = 0;
-  digit = 0;
+a = 0;
+b = 0;
+c = 0;
+d = 0;
+e = 0;
+digit = 0;
 
-  while (s[d] != '\0')
-    d ++;
-  while(a < d && e == 0)
-    {
-      if (s[a] == '-')
-	++b;
-      if (s[a] >= '0' && s[a] <= '9')
-	{
-	  digit = s[a] - '0';
-	  if (b % 2)
-	    digit = -digit;
-	  c = c * 10 + digit;
-	  e = 1;
-	    if (s[a + 1] < '0' || s[a + 1] > '9')
-	      break;
-	    e = 0;
-		}
-      a++;
-	}
-  if (e == 0)
-    return(0);
+while (s[d] != '\0')
+d ++;
+while(a < d && e == 0)
+{
+if (s[a] == '-')
+++b;
+if (s[a] >= '0' && s[a] <= '9')
+{
+ digit = s[a] - '0';
+if (b % 2)
+digit = -digit;
+c = c * 10 + digit;
+e = 1;
+if (s[a + 1] < '0' || s[a + 1] > '9')
+break;
+e = 0;
+}
+a++;
+}
+if (e == 0)
+return(0);
 
-      return(c);
+return(c);
 }
