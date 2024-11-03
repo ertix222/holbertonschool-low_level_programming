5366d47254f809ea04c9b6fbd0bb35225dbf05fc
ertix222 <ertix222@gmail.com>
2024-11-03 21:32:36 +0000

    toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *string toupper- change lowercase tu uppercase
+ * @s: modified string
+ *Return: the result of the change
+ */
+
+char *string_toupper(char *s)
+
+
+{
+int a = 0;
+
+while (s[a] != '\0')
+{
+
+if (s[a] >= 'a' && s[a] <= 'z')
+{
+
+s[a] = s[a] - 32;
+}
+
+i++
+}
+return s;
+}
