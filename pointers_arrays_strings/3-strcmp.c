25235c83e14bf249b278badb024bd66930b051df
ertix222 <ertix222@gmail.com>
2024-11-03 19:30:42 +0000

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,8 @@
  *_strcmp- concatenates two string in one
  * @s1: first string
  * @s2: second string
- *Return: 0 if they're equal, less if s1 if less s2, more if s2 is greater than s1
+ *Return: 0 if they're equal, less if s1 if less s2,
+ * more if s2 is greater than s1
  */
 
 int _strcmp(char *s1, char *s2)
