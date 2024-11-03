7f4a41f10f56fd2b2e6aaaed4bdb15a40598e900
ertix222 <ertix222@gmail.com>
2024-11-03 22:20:33 +0000

    cap string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ *cap_string- capatalize word
+ *Return:
+ */
+
+char *cap_string(char *s)
+
+{
+int a = 0;
+int b = 0;
+
+char separators[13] = " \t\n,;.!?\"(){}";
+
+
+if (s[a] >= 'a' && s[a] <= 'z')
+{
+s[a] = s[a] - 32;
+}
+i++;
+
+while (s[a] != '\0')
+{
+  for (b = 0; separators[b] != '\0'; b++)
+    {
+
+     if (s[a - 1] == separators[b] && s[a] >= 'a' && s[a] <= 'z')
+       {
+	 s[a] = s[a] - 32;
+
+
+ break;
+       }
+    }
+i++;
+
+ }
+return (s);
+}
