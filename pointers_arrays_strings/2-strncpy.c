299f72699db8b76e7b91844f2ebdcccb1464a1ae
ertix222 <ertix222@gmail.com>
2024-11-03 17:40:21 +0000

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *_strcat- concatenates two string in one
+ * @dest: a pointer to the destination which must be sufficiently large
+ * @src: pointer to source string
+ * @n: maximum number of caracter to copy of src
+ *Return: a pointer to the destination string after the
+ * concatenation.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+int a = 0;
+
+while (src[a] != '\0' && i < n)
+{
+dest[a] = src[a];
+a++;
+}
+
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
+return (dest);
+}
