18e435552ef46331f18a92d84a72a082d5a1fd4a
ertix222 <ertix222@gmail.com>
2024-11-04 23:29:09 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,31 +3,33 @@
 
 /**
  * _strspn- get the length of a prefix substring
- *@s:
- *@accept: 
+ *@s: chain who we want the length
+ *@accept: only caractere in accept are considerete
  *Return: maybe
  */
 
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
 
 
 {
- unsigned int count = 0;
+unsigned int count = 0;
+int a, b;
+ 
+for (a = 0; s[a] != '\0'; a++)
+{
+int found = 0;
 
- int a, b;
-
- for (a = 0; s[a] != '\0'; a++)
-   {
-     int found = 0;
-
-     for (b = 0; accept[b] != '\0';b++)
-       {
-	 if (s[a] == accept[b])
-	   {
-	   count++
-	     }
-	 found = 1;
-       }
-	 break;
-   }
-  }
+for (b = 0; accept[b] != '\0'; b++)
+{
+if (s[a] == accept[b])
+{
+count++;
+found = 1;
+break;
+}
+}
+if (!found)
+break;
+}
+return count;
+}
