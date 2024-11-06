fe84a9736cd218eb47ac75f758e5bd3a3e885b3d
ertix222 <ertix222@gmail.com>
2024-11-06 13:46:11 +0000

    strlen in recursion

--- recursion/2-main.c
+++ recursion/2-main.c
@@ -1,7 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strlen_recursion- length of a string
- *@s: the string who will be calculate
- *Return: always
+ * main - check the code
+ *
+ * Return: Always 0.
  */
+int main(void)
+{
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}
