0c51a466106e3d11b004e0c7c77c34c25060bd97
ertix222 <ertix222@gmail.com>
2024-11-06 13:44:32 +0000

    strlen in recursion

---
+++ recursion/2-main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion- length of a string
+ *@s: the string who will be calculate
+ *Return: always
+ */
---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion- length of a string
+ *@s: the string who will be calculate
+ *Return: always
+ */
+
+int _strlen_recursion(char *s)
+
+{
+
+if (*s == '\0')
+{
+
+return(0);
+}
+
+return (1 + _strlen_recursion(s + 1));
+}
