58b384f0e7a14af825bc28cd10fe07f6cb43f51c
ertix222 <ertix222@gmail.com>
2024-11-07 16:19:44 +0000

    inception

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * prime_chek- recursion who will check if it's a prime number with modulo
+ *@n: number give in is_prime_number 
+ *@guess: number to find if it's a prime one or not
+ *Return: maybe
+ */
+
+int prime_chek(int n, int guess)
+
+{
+  if (guess %
+      return (
+  if (
+
+
+
+
+
+}
+/**
+ * is_prime_number- function that returns 1 if it's a prime number
+ * otherwise it will return 0.
+ *@n: number given
+ *Return: 1 if n is prime else 0
+ */
+
+
+int is_prime_number(int n)
+{
+
+if (n != prime)
+return (0);
+
+if (n = prime)
+return (1);
+
+return (prime_chek(n, 0));
+
+}
