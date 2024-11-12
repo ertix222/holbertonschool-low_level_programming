f559e12eb79ac7d63902d2648cb1677581e606d5
ertix222 <ertix222@gmail.com>
2024-11-12 14:24:26 +0000

    concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat- a function that concatenates two strings
+ *@s1
+ *@s2
+ *Return: concatenate
+ */
+
+char *str_concat(char *s1, char *s2)
