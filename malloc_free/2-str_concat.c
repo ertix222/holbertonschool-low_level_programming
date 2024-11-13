c757dbd6d45d80e10be214eaa7b9ff7e6a293fd9
ertix222 <ertix222@gmail.com>
2024-11-13 00:17:35 +0000

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,38 +4,42 @@
 
 /**
  * str_concat- a function that concatenates two strings
- *@s1
- *@s2
+ *@s1: first part
+ *@s2: second part
  *Return: concatenate
  */
 char *str_concat(char *s1, char *s2)
 
 {
-int a, b , i;
-
+int a, b, i;
+char *s3;
 a = 0;
 b = 0;
 
 if (s1 != NULL)
 {
-while(s1[a] != '\0')
+while (s1[a] != '\0')
 a++;
 }
-
 if (s2 != NULL)
 {
-while(s2[b] != '\0')
+while (s2[b] != '\0')
 b++;
 }
 
-s3 = (char *)malloc((a + b + 1) * sizeof(char))
+s3 = (char *)malloc((a + b + 1) * sizeof(char));
+if (s3 == NULL)
+{
+return (NULL);
+}
 for (i = 0; i < a; i++)
 {
-s3[i] = s1[i]
+s3[i] = s1[i];
 }
-for (i = 0; i < b; b++)
+for (i = 0; i < b; i++)
 {
-s3[i + a] = s2[i]
+s3[i + a] = s2[i];
 }
-s3[a + b] = '\0'
+s3[a + b] = '\0';
 return (s3);
+}
