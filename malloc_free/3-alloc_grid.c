6d566df1b45d59f009e8d467de91cc7493aa8379
ertix222 <ertix222@gmail.com>
2024-11-13 00:24:47 +0000

    grid

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int **alloc_grid(int width, int height)
+
+  int **grid;
+int i, j;
+
+if (with <= 0 || height <= 0)
+  {
+    return (NULL);
+  }
