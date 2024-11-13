b79e6700469c72b8d8ceb7b1473a61cd1c98851b
ertix222 <ertix222@gmail.com>
2024-11-13 22:46:54 +0000

    nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,12 +3,42 @@
 
 /**
  * string_nconcat- 
- *@s1:
- *@s2:
- *@n:
- *Return: 
+ *@s1: first chain
+ *@s2: second chain
+ *@n: number of carac of the s2 to include
+ *Return: pointer to the concatenated string
  */
 
 char *string_nconcat(char*s1,  char *s2, unsigned int n)
 
 {
+
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+
+ 
+unsigned int l1 = strlen(s1);
+unsigned int l2 = strlen(s2);
+unsigned int num_we_copy  = (n < l2) n : l2;
+
+
+char *new_str = malloc(l1 + num_we_copy + 1);
+
+
+if (new_str == NULL)
+{
+return (NULL);
+}
+
+strcpy(new_str,  s1);
+strncat(new_str, s2, num_we_copy);
+
+return new_str;
+ 
+}
