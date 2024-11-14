36c116968cbc8c2541b231065ea88a09f86de499
ertix222 <ertix222@gmail.com>
2024-11-14 10:18:11 +0000

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -6,7 +6,6 @@
  * _calloc - Allocates memory for an array and initializes to zero.
  * @nmemb: Number of elements.
  * @size: Size in bytes of each element.
- *
  * Return: A pointer to the allocated memory, or NULL on failure.
  */
 
@@ -14,13 +13,14 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
 unsigned int total_size;
+unsigned int i = 0;
+char *temp;
 void *memory;
 
 if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
-
 total_size = nmemb * size;
 
 memory = malloc(total_size);
@@ -28,6 +28,11 @@ if (memory == NULL)
 {
 return (NULL);
 }
-memset(memory, 0, total_size);
+
+temp = (char *)memory;
+for (i = 0; i < total_size; i++)
+{
+temp[i] = 0;
+}
 return (memory);
 }
