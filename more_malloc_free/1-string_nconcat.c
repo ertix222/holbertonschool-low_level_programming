55bc7b6516904a1a2c142a965c5e9281035d2d8d
ertix222 <ertix222@gmail.com>
2024-11-14 00:14:20 +0000

    nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,35 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * string_nconcat- 
+ * string_nconcat- concatenated two string
  *@s1: first chain
  *@s2: second chain
  *@n: number of carac of the s2 to include
  *Return: pointer to the concatenated string
  */
 
-char *string_nconcat(char*s1,  char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
+unsigned int l1, l2, num_we_copy;
+char *new_str;
 
 if (s1 == NULL)
-{
 s1 = "";
-}
 if (s2 == NULL)
-{
 s2 = "";
-}
 
- 
-unsigned int l1 = strlen(s1);
-unsigned int l2 = strlen(s2);
-unsigned int num_we_copy  = (n < l2) n : l2;
+l1 = strlen(s1);
+l2 = strlen(s2);
+num_we_copy  = (n < l2) ? n : l2;
 
-
-char *new_str = malloc(l1 + num_we_copy + 1);
-
+new_str = malloc(l1 + num_we_copy + 1);
 
 if (new_str == NULL)
 {
@@ -39,6 +35,6 @@ return (NULL);
 strcpy(new_str,  s1);
 strncat(new_str, s2, num_we_copy);
 
-return new_str;
- 
+return (new_str);
+
 }
