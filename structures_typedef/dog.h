0b60bb13c61a20f86bf98bc4febe19c082ef2b44
ertix222 <ertix222@gmail.com>
2024-11-15 11:57:01 +0100

    poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog- strucuture that representing
+*@name: name of the doggo
+*@age: age of the doggo
+*@owner: owner of the doggo
+*/
 struct dog
 {
 char *name;
