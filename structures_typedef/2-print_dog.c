05655ec9b80032c2d26a5859d99e0041e18cdc52
ertix222 <ertix222@gmail.com>
2024-11-15 20:42:06 +0100

    print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ return;
 
 printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner ? d->name: "(nil)"));
+printf("Owner: %s\n", d->owner ? d->name: "(nil)");
 
 }
