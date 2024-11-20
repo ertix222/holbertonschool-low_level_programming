b9185d9eff07c76841ee1ee19e422c8eeed5e8b0
ertix222 <ertix222@gmail.com>
2024-11-20 11:03:56 +0100

    t3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "3-calc.c"
+ 
+
+//atoi pour parsser les agrv en int
+//loop interdit
+//3 if au max
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+*This file should contain the 5 following functions (not more)
+* op_add- returns the sum of a and b
+*@a: number to add
+*@b: number to add
+*/
+int op_add(int a, int b);
+{
+return a + b;
+}
+
+/**
+* op_sub- returns the difference of a and b
+*@a: number to sub
+*@b: number to sub
+*/
+int op_sub(int a, int b);
+{
+return a - b;
+}
+
+/**
+* op_mul- returns the product of a and b
+*@a: number to mul
+*@b: number to mul
+*/
+int op_mul(int a, int b);
+{
+return a * b;
+}
+
+/**
+* op_div- returns the result of the division of a by b
+*@a: number to div
+*@b: number to div
+*/
+int op_div(int a, int b);
+{
+return a / b;
+}
+
+/**
+* op_mod- returns the remainder of the division of a by b
+*@a: number to mod
+*@b: number to mod
+*/
+int op_mod(int a, int b);
+{
+return a % b;
+}
