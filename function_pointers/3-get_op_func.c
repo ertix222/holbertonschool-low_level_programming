f7ef2181093cd2786fc5af6b7245c7edaf4dd4db
ertix222 <ertix222@gmail.com>
2024-11-20 16:11:23 +0100

    update get op

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -5,17 +5,29 @@
 * get_op_func- function that selects the correct function,
 * to perform the operation asked by the user.
 *@s: the operator passed as argument to the program
-*
+*Return: the function that corresponds to the operator given as a parameter
 */
 
 int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = 
+{
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
 
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+while (ops[i].op != NULL)
+{
+if (ops[i].op[0] == s[0] && s[1] == '\0')
+{
+return (ops[i].f);
+}
+i++;
+}
+return (NULL);
+}
