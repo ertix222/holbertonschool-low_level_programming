583627ede3b2a480b6f1130b412b42a6bbfffa42
ertix222 <ertix222@gmail.com>
2024-11-22 10:15:34 +0100

    fix compl error due to a ;

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,6 @@
 #define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
-void print_number(const char *separator, const unsigned int n, ...)
+void print_number(const char *separator, const unsigned int n, ...);
 
 #endif
