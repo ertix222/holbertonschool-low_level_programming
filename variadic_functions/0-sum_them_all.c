5978a5bc4be0b8a1a813586276244c81a307e2ee
ertix222 <ertix222@gmail.com>
2024-11-22 10:12:54 +0100

    fix c90 and ;

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,17 +12,18 @@
 int sum_them_all(const unsigned int n, ...)
 
 {
-int sum = 0, total = 0;
+int total = 0;
 unsigned int i = 0;
+va_list args;
+
 if (n == 0)
 return (0);
 
-va_list args;
 va_start (args, n);
 
 for (; i < n; i++)
 {
-    total += va_arg(args, int)
+    total += va_arg(args, int);
 }
 va_end(args);
 return (total);
