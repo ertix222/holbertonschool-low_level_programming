f069b0a210ab45fe38d26848c47c17b1186c384c
ertix222 <ertix222@gmail.com>
2024-12-04 08:35:49 +0100

    print list comment

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+* print_list- 
+*
+*Return: the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+
+{
+size_t count = 0; /*counter for nodes*/
+const list_t *current;/*temp pointer to travers the list*/
+while(current != NULL)
+{
+if (current->str == NULL)
+{
+    printf("[0] (nil)");
+}
+else{
+    printf("[%lu] %s\n", strlen(current->str), current->str);
+}
+count++;
+current = current->next;/*go to the next node*/
+}
+    return (count); /*number of nods*/
+}
