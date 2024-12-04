7beb3476cec31d8d2e68706d3ac10c05c8ffbbc7
ertix222 <ertix222@gmail.com>
2024-12-04 15:40:13 +0100

    adding the lenght of node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,22 +13,26 @@ list_t *add_node(list_t **head, const char *str)
 
 {
 list_t *new;
+size_t lenght = 0;
 
 if (head == NULL || str == NULL)
-{
 return (NULL);
-}
+
 new = malloc(sizeof(list_t));
 if (new == NULL)
-{
 return (NULL);
-}
+
 new->str = strdup(str);
 if (new->str == NULL)
 {
 free(new);
 return (NULL);
 }
+while (str[lenght] != '\0')
+{
+lenght++;
+}
+new->len = lenght;
 new->next = *head;
 *head = new;
 return (new);
