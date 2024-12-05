2afd7374536f50d43141d328e282c446b9bc1a2a
ertix222 <ertix222@gmail.com>
2024-12-05 02:35:34 +0100

    compliation good anwser good

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint- print all element of a list
+ * @h: pointer to the header of the file
+ * Return:  total number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+
+{
+size_t count = 0; /*count node*/
+const dlistint_t *current = h;
+
+if (h == NULL)
+return (0);
+
+while (current != NULL)
+{
+printf("%d\n", current->n);
+count++;
+current = current->next;
+}
+return(count);
+}
